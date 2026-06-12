9611b4064ec1c59a3570b1f9a8b85c782217acf6
Anh Duc Phan <phanduc1809@gmail.com>
2026-06-12 15:17:01 +0700

    add more info to the screen

---
+++ lib/account_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class AccountScreen extends StatelessWidget {
+  const AccountScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const Color bgColor = Color(0xFF141415);
+
+    return Scaffold(
+      backgroundColor: bgColor,
+      appBar: AppBar(
+        backgroundColor: bgColor,
+        title: Text(
+          'Tài Khoản',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.person, size: 64, color: Colors.grey[800]),
+            const SizedBox(height: 16),
+            const Text(
+              'Trang tài khoản đang được xây dựng...',
+              textAlign: TextAlign.center,
+              style: TextStyle(color: Colors.grey, fontSize: 16),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
